80252b8a50a50d4f1551877db3e0c7905816b0d9
YeeHong-kyc <yeehongchee@gmail.com>
2025-04-16 19:17:34 +0800

    Add: database but not susccesfully complete

--- server.py
+++ server.py
@@ -1,4 +1,19 @@
+import sqlite3
 from flask import Flask, render_template
+
+def create_db():
+    conn = sqlite3.connect('mmu.db')  
+    c = conn.cursor()
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            email TEXT NOT NULL,
+            password TEXT NOT NULL
+        )
+    ''')
+    conn.commit()
+    conn.close()
+    print(" Database and 'users' table created successfully.")
 
 app = Flask(__name__)
 
